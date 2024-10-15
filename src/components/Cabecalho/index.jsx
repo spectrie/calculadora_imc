17bd38b163e299488537f7e971ee9c2454254710
Spectrie <victor.campbell10@outlook.com>
2024-10-14 21:37:37 -0300

    4commit

--- src/components/Cabecalho/index.jsx
+++ src/components/Cabecalho/index.jsx
@@ -4,7 +4,9 @@ const Cabecalho = () => {
     return(
     <header className={styles.header}>
         <img className={styles.avatar} src="https://cdn-icons-png.flaticon.com/512/1668/1668490.png" alt="" />
-        <h1>Calculadora IMC</h1>
+        <h1>
+            Calculadora IMC
+            </h1>
     </header>
     )
 }
